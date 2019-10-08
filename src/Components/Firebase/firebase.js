b14188d5dcfe966cd0cd9fb2f096911ba1afe815
boo182 <lucien.ladurie@gmail.com>
2019-10-09 00:17:10 +0200

    Hide config

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -5,12 +5,7 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyAIId2ByN9c0RX9_R71EbJJoV-lB1RDADc",
-  authDomain: "test-technique-beeldi.firebaseapp.com",
-  databaseURL: "https://test-technique-beeldi.firebaseio.com",
-  projectId: "test-technique-beeldi",
-  storageBucket: "test-technique-beeldi.appspot.com",
-  messagingSenderId: "937748581892"
+
 };
 
 if (!firebase.apps.length) {
